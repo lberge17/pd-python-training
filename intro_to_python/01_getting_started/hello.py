cd360c6eb6703e9a8564d2a1f32c8f74af02ef8b
Laura Berge <lauraeberge@gmail.com>
2022-02-28 15:46:20 -0600

    add code from study group

---
+++ intro_to_python/01_getting_started/hello.py
@@ -0,0 +1,31 @@
+# print("Hi", end=None)
+# print("Hello")
+# print("hi", 'my', 'name', 'is', sep='-')
+
+# a, b, c = 4, 6, 10
+
+x = y = z = {'name': 'Laura'}
+
+me = {'firstName': 'Joe'}
+you = {'firstName': 'Joe'}
+
+x = 15
+y = 15
+
+# MAX_N = 30
+# print(MAX_N)
+# MAX_N = 60
+# print(MAX_N)
+import pdb; pdb.set_trace()
+
+
+# def function():
+    # code here
+
+
+# x = 15 (1111)
+# y = 4  (1111)
+
+# x //= 4
+# print(x is y)
+
